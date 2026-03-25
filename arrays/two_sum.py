567f1f7e85a753a143d389392f6376931560956d
Abu Bakar Siddik <jobayeralnoman1@gmail.com>
2026-03-25 20:00:08 +0800

    two_sum.py

---
+++ arrays/two_sum.py
@@ -0,0 +1,19 @@
+# Problem: Two Sum
+# Find two numbers that add up to target
+
+nums = list(map(int, input("Enter numbers: ").split()))
+target = int(input("Enter target: "))
+
+found = False
+
+for i in range(len(nums)):
+    for j in range(i + 1, len(nums)):
+        if nums[i] + nums[j] == target:
+            print(f"Pair found: {nums[i]}, {nums[j]}")
+            found = True
+            break
+    if found:
+        break
+
+if not found:
+    print("No pair found")
